2fd76c7f19ab91d66f314e0b1e6e3474a1ea8686
m-michrov <45308469+m-michrov@users.noreply.github.com>
2019-02-08 20:25:58 +0700

    cmake folders

---
+++ cmake-build-debug/CMakeFiles/AVLTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AVLTree.dir/main.c.obj"
+  "AVLTree.pdb"
+  "AVLTree.exe"
+  "AVLTree.exe.manifest"
+  "libAVLTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AVLTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-default/CMakeFiles/AVLTree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AVLTree.dir/main.c.obj"
+  "CMakeFiles/AVLTree.dir/avltree.c.obj"
+  "AVLTree.pdb"
+  "AVLTree.exe"
+  "AVLTree.exe.manifest"
+  "libAVLTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AVLTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
